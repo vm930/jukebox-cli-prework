999dcb49d0e77a1e17e0420b0f99e76b17a40d20
abstract-animation-9914 <vm930@hotmail.com>
2019-02-14 15:21:43 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,59 +14,82 @@ songs = [
 ]
 
 def help
-  help = <<-HELP
-I accept the following commands:
+  puts "I accept the following commands:
 - help : displays this help message
 - list : displays a list of songs you can play
 - play : lets you choose a song to play
-- exit : exits this program
-HELP
-
-  puts help
-end
+- exit : exits this program"
+end 
 
 def list(songs)
-  songs.each_with_index do |song, i|
-    puts "#{i+1}. #{song}"
+  songs.each_with_index do |list, index|
+    puts "#{index+1} #{list}"
   end
-end
+end 
 
 def play(songs)
   puts "Please enter a song name or number:"
-  song_to_play = gets.chomp
-  if (1..9).to_a.include?(song_to_play.to_i)
-    puts "Playing #{songs[song_to_play.to_i - 1]}"
-  elsif songs.include?(song_to_play)
-    puts "Playing #{song_to_play}"
-  else
+  input = gets.chomp
+  if [1,2,3,4,5,6,7,8,9].include?(input.to_i)
+    puts "Playing #{songs[input.to_i-1]}"
+    elsif songs.include?(input)
+    puts "Playing #{input}"
+  else 
     puts "Invalid input, please try again"
   end
-end
+end 
 
 def exit_jukebox
   puts "Goodbye"
-end
+end 
 
 def run(songs)
-  # help
-
+  help
   input = ""
   while input
-    puts "Please enter a command:"
-    input = gets.downcase.strip
-    case input
-    when 'list'
-      list(songs)
-    when 'play'
-      list(songs)
-      play(songs)
-    when 'help'
-      help
-    when 'exit'
-      exit_jukebox
-      break
-    else
-      help
-    end
-  end
-end
+  puts "Please enter a command:"
+  input = gets.downcase.chomp
+      case input 
+      when "play"
+        play(songs)
+      when "list"
+        list(songs)
+        play(songs)
+      when "help"
+        help
+      when "exit"
+        exit_jukebox
+        break
+      else 
+        help
+      end 
+  end 
+end 
+
+
+
+
+
+# def run(songs)
+#   # help
+
+#   input = ""
+#   while input
+#     puts "Please enter a command:"
+#     input = gets.downcase.strip
+#     case input
+#     when 'list'
+#       list(songs)
+#     when 'play'
+#       list(songs)
+#       play(songs)
+#     when 'help'
+#       help
+#     when 'exit'
+#       exit_jukebox
+#       break
+#     else
+#       help
+#     end
+#   end
+# end
